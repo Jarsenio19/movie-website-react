906a24d009742e61caf6eb32743809051fffcd4b
Phil <pjguitar15@gmail.com>
2026-03-30 21:32:05 +0800

    added empty space

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Search />} />
         <Route path='/Home' element={<Home />} />
+        
       </Routes>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Header from '../components/Header/Header'
 const Home = () => {
   return (
     <>
+    
       <Header />
       <Carousel />
       <SuggestionCard />
