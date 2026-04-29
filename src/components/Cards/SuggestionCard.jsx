9cb65ecbee169bb394bae126d1abc4119db17ddd
Jayson Arsenio <jay.arsenio19@gmail.com>
2026-04-29 21:03:32 +0800

    Aditional

--- src/components/Cards/SuggestionCard.jsx
+++ src/components/Cards/SuggestionCard.jsx
@@ -2,33 +2,13 @@ import MovieCard from './MovieCard.jsx'
 import './Cards.css'
 import { MovieData } from '../../Data/MovieData.js'
 import { useEffect, useState } from 'react'
+import Home from '../../pages/Home.jsx'
 
 const SuggestionCard = () => {
-  const [movies, setMovies] = useState([])
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
-  useEffect(() => {
-    const fetchAPI = async () => {
-      try {
-        setLoading(true)
-        const jsonResponse = await fetch('https://api.imdbapi.dev/titles')
-        const result = await jsonResponse.json()
-        const buildMovies = result.titles.map((item) => ({
-          id: item.id,
-          title: item.primaryTitle,
-          image: item.primaryImage.url,
-          ...item,
-        }))
-        setMovies(buildMovies)
-      } catch (error) {
-        setError('Something went wrong while fetching movie.')
-      }
-      setLoading(false)
-    }
-    fetchAPI()
-  }, [])
 
-  const suggestMovies = movies.filter((movie) => movie.rating?.aggregateRating > 8)
+  <Home />
+
+
 
 
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,44 @@ import SuggestionCard from '../components/Cards/SuggestionCard'
 import LatestMoviesCard from '../components/Cards/LatestMoviesCard'
 import LatestTvSeries from '../components/Cards/LatestTvSeries'
 import Footer from '../components//Footer/Footer'
+import { useEffect, useState } from 'react'
 
 
 const Home = () => {
+
+  const [movies, setMovies] = useState([])
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+
+    const fetchAPI = async () => {
+      try {
+        setLoading(true)
+        const jsonResponse = await fetch('https://api.imdbapi.dev/titles')
+        const result = await jsonResponse.json()
+        const buildMovies = result.titles.map((item) => ({
+          id: item.id,
+          title: item.primaryTitle,
+          image: item.primaryImage.url,
+          ...item,
+        }))
+        setMovies(buildMovies)
+      } catch (error) {
+        setError('Something went wrong while fetching movie.');
+      }
+      setLoading(false)
+    }
+    fetchAPI()
+  }, [])
+
+
+
+  // DOUBLE CHECK!!!
   return (
     <>
       <Header />
       <Carousel />
-      <SuggestionCard />
+      <SuggestionsCard movies={movies} />
       <LatestMoviesCard />
       <LatestTvSeries />
       <Footer />
