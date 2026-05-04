49659298d1b9e8694d26798bcf2f7cd54f2865c2
Jayson Arsenio <jay.arsenio19@gmail.com>
2026-05-04 17:43:27 +0800

    fix: responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,7 +38,6 @@ const Header = () => {
                         <div className="primary-logo">
                             <Link to='/' >JAYSONMOVIES</Link>
                         </div>
-
                         <button
                             className="mobile-nav-toggle"
                             onClick={toggleMenu}
@@ -46,52 +45,52 @@ const Header = () => {
                         >
                             <img className="icon-hamburger" src="../public/menus.png" alt="" aria-hidden="true" />
                         </button>
-
-                        <nav className={`primary-navigation 
-                            ${isOpen ? 'active' : ''}`}
-                            id="primary-navigation">
-
-                            <ul aria-label="Primary" role="list" className="nav-list">
-
-                                <Link to='/home'> <li>Home</li></Link>
-
-                                <li
-                                    className={`mega-parent ${isGenresOpen ? 'active' : ''}`}
-                                    onClick={toggleGenres}> <span>Genres</span>
-                                    <div className="mega-menu" id="mega-menu">
-                                        <div className="mega-column">
-                                            <Link to="/genre/action">Action</Link>
-                                            <Link to="/genre/adventure-action">Action & Adventure</Link>
-                                            <Link to="/genre/animation">Animation</Link>
-                                            <Link to="/genre/biography">Biography</Link>
-                                            <Link to="/genre/comedy">Comedy</Link>
-                                            <Link to="/genre/crime">Crime</Link>
-                                            <Link to="/genre/drama">Drama</Link>
-                                        </div>
-                                        <div className="mega-column">
-                                            <Link to="/genre/family">Family</Link>
-                                            <Link to="/genre/fantasy">Fantasy</Link>
-                                            <Link to="/genre/history">History</Link>
-                                            <Link to="/genre/horror">Horror</Link>
-                                            <Link to="/genre/music">Music</Link>
-                                            <Link to="/genre/mystery">Mystery</Link>
-                                        </div>
-                                        <div className="mega-column">
-                                            <Link to="/genre/romance">Romance</Link>
-                                            <Link to="/genre/sci-fi">Sci-Fi</Link>
-                                            <Link to="/genre/sport">Sport</Link>
-                                            <Link to="/genre/thriller">Thriller</Link>
-                                            <Link to="/genre/western">Western</Link>
-                                        </div>
-                                    </div>
-                                </li>
-                                <Link to='/movies'><li>Movies</li></Link>
-                                <Link to='/tvseries'> <li>TV-Series</li></Link>
-                                <Link to='/topimdb'>  <li>Top IMDb</li></Link>
-                            </ul>
-                        </nav>
-
                     </div>
+
+                    <nav className={`primary-navigation 
+                            ${isOpen ? 'active' : ''}`}
+                        id="primary-navigation">
+
+                        <ul aria-label="Primary" role="list" className="nav-list">
+
+                            <Link to='/home'> <li>Home</li></Link>
+
+                            <li
+                                className={`mega-parent ${isGenresOpen ? 'active' : ''}`}
+                                onClick={toggleGenres}> <span>Genres</span>
+                                <div className="mega-menu" id="mega-menu">
+                                    <div className="mega-column">
+                                        <Link to="/genre/action">Action</Link>
+                                        <Link to="/genre/adventure-action">Action & Adventure</Link>
+                                        <Link to="/genre/animation">Animation</Link>
+                                        <Link to="/genre/biography">Biography</Link>
+                                        <Link to="/genre/comedy">Comedy</Link>
+                                        <Link to="/genre/crime">Crime</Link>
+                                        <Link to="/genre/drama">Drama</Link>
+                                    </div>
+                                    <div className="mega-column">
+                                        <Link to="/genre/family">Family</Link>
+                                        <Link to="/genre/fantasy">Fantasy</Link>
+                                        <Link to="/genre/history">History</Link>
+                                        <Link to="/genre/horror">Horror</Link>
+                                        <Link to="/genre/music">Music</Link>
+                                        <Link to="/genre/mystery">Mystery</Link>
+                                    </div>
+                                    <div className="mega-column">
+                                        <Link to="/genre/romance">Romance</Link>
+                                        <Link to="/genre/sci-fi">Sci-Fi</Link>
+                                        <Link to="/genre/sport">Sport</Link>
+                                        <Link to="/genre/thriller">Thriller</Link>
+                                        <Link to="/genre/western">Western</Link>
+                                    </div>
+                                </div>
+                            </li>
+                            <Link to='/movies'><li>Movies</li></Link>
+                            <Link to='/tvseries'> <li>TV-Series</li></Link>
+                            <Link to='/topimdb'>  <li>Top IMDb</li></Link>
+                        </ul>
+                    </nav>
+
                     <SearchInput />
 
 
